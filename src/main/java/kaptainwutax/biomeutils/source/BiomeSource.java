bf15c01e131a02bc72963c474842e58a3550b771
KaptainWutax <kaptainwutax@gmail.com>
2020-06-03 15:19:56 -0400

    added biome source iteration

--- src/main/java/kaptainwutax/biomeutils/source/BiomeSource.java
+++ src/main/java/kaptainwutax/biomeutils/source/BiomeSource.java
@@ -3,6 +3,10 @@ package kaptainwutax.biomeutils.source;
 import kaptainwutax.biomeutils.Biome;
 import kaptainwutax.seedutils.mc.MCVersion;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
 public abstract class BiomeSource {
 
 	private final MCVersion version;
@@ -22,5 +26,41 @@ public abstract class BiomeSource {
 	}
 
 	public abstract Biome getBiome(int blockX, int blockY, int blockZ);
+
+	public void iterateUniqueBiomes(int x, int y, int z, int radius, BiomeSupplier supplier, Predicate<Biome> shouldContinue) {
+		int i = x - radius >> 2;
+		int j = y - radius >> 2;
+		int k = z - radius >> 2;
+		int l = x + radius >> 2;
+		int m = y + radius >> 2;
+		int n = z + radius >> 2;
+		int o = l - i + 1;
+		int p = m - j + 1;
+		int q = n - k + 1;
+
+		Set<Integer> set = new HashSet<>();
+
+		for(int r = 0; r < q; ++r) {
+			for(int s = 0; s < o; ++s) {
+				for(int t = 0; t < p; ++t) {
+					int u = i + s;
+					int v = j + t;
+					int w = k + r;
+
+					Biome b = supplier.getBiome(u, v, w);
+
+					if(!set.contains(b.getId())) {
+						if(!shouldContinue.test(b))return;
+					}
+
+					set.add(b.getId());
+				}
+			}
+		}
+	}
+
+	public interface BiomeSupplier {
+		Biome getBiome(int x, int y, int z);
+	}
 
 }
