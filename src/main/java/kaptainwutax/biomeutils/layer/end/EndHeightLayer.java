18d07107fea221a6e843a99bc4419bc7e3bc7b7b
Neil <h66r10@gmail.com>
2021-03-18 23:11:41 +0100

    fix(end): floating point errors

--- src/main/java/kaptainwutax/biomeutils/layer/end/EndHeightLayer.java
+++ src/main/java/kaptainwutax/biomeutils/layer/end/EndHeightLayer.java
@@ -11,6 +11,11 @@ public class EndHeightLayer extends BiomeLayer {
 
     @Override
     public int sample(int x, int y, int z) {
+        float height=getNoiseValueAt(x,z);
+        return Float.floatToIntBits(height);
+    }
+
+    public float getNoiseValueAt(int x, int z){
         int scaledX = x / 2;
         int scaledZ = z / 2;
         int oddX = x % 2;
@@ -20,10 +25,10 @@ public class EndHeightLayer extends BiomeLayer {
 
         for(int rx = -12; rx <= 12; ++rx) {
             for(int rz = -12; rz <= 12; ++rz) {
-                int shiftedX = scaledX + rx;
-                int shiftedZ = scaledZ + rz;
+                long shiftedX = scaledX + rx; // this is under 32 bits since the max is 15M+12 at max
+                long shiftedZ = scaledZ + rz;
                 // TODO make it version dependant (bug in MC)
-                if (shiftedX * shiftedX + shiftedZ * shiftedZ > 4096L && this.getParent().get(shiftedX, 0, shiftedZ) == 1) {
+                if (shiftedX * shiftedX + shiftedZ * shiftedZ > 4096L && this.getParent().get((int)shiftedX, 0, (int) shiftedZ) == 1) {
                     float elevation = (Math.abs((float)shiftedX) * 3439.0F + Math.abs((float)shiftedZ) * 147.0F) % 13.0F + 9.0F;
                     float smoothX = (float)(oddX - rx * 2);
                     float smoothZ = (float)(oddZ - rz * 2);
@@ -33,8 +38,7 @@ public class EndHeightLayer extends BiomeLayer {
                 }
             }
         }
-
-        return Float.floatToIntBits(height);
+        return height;
     }
 
     protected static float clamp(float value) {
