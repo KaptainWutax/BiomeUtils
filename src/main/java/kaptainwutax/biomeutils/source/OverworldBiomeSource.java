742860d4b41737d9907f012c5ca69155b42cdef7
Neil <h66r10@gmail.com>
2021-03-30 00:42:32 +0200

    feat(support 1.5.2)

--- src/main/java/kaptainwutax/biomeutils/source/OverworldBiomeSource.java
+++ src/main/java/kaptainwutax/biomeutils/source/OverworldBiomeSource.java
@@ -49,10 +49,10 @@ public class OverworldBiomeSource extends BiomeSource {
     public OverworldBiomeSource(MCVersion version, long worldSeed, int biomeSize, int riverSize) {
         super(version, worldSeed);
 
-        if (this.getVersion().isOlderThan(MCVersion.v1_6_1)) {
+        if (this.getVersion().isOlderThan(MCVersion.v1_15_2)) {
             throw new UnsupportedVersion(this.getVersion(), "overworld biomes");
         }
-        if (this.getVersion().isOlderThan(MCVersion.v1_6_1)) {
+        if (this.getVersion().isOlderThan(MCVersion.v1_15_2)) {
             System.out.println("WARNING USING TEMPORARY BIOME STACK (NOT VERIFIED)");
         }
 
