b6ec4b722b0b5bfa1e730a031660e3b66ede2a70
Neil <h66r10@gmail.com>
2021-04-23 14:08:08 +0200

    sample multi layer

--- src/main/java/kaptainwutax/biomeutils/layer/IntBiomeLayer.java
+++ src/main/java/kaptainwutax/biomeutils/layer/IntBiomeLayer.java
@@ -1,7 +1,11 @@
 package kaptainwutax.biomeutils.layer;
 
 import kaptainwutax.biomeutils.layer.cache.IntLayerCache;
+import kaptainwutax.biomeutils.layer.composite.VoronoiLayer;
+import kaptainwutax.biomeutils.layer.scale.ScaleLayer;
 import kaptainwutax.mcutils.version.MCVersion;
+
+import java.rmi.UnexpectedException;
 
 public abstract class IntBiomeLayer extends BiomeLayer {
 
@@ -33,8 +37,47 @@ public abstract class IntBiomeLayer extends BiomeLayer {
 
 	public abstract int sample(int x, int y, int z);
 
+	public void _sample(int x,int z,int xSize,int zSize) throws UnexpectedException {
+		if (this.getParents()== null){
+			throw new UnexpectedException("Error, in our model, this should not happen (we use a null parent array)");
+		}
+		for (BiomeLayer biomeLayer : this.getParents()) {
+			int shift = 0;
+			if (this instanceof ScaleLayer) shift = 1;
+			else if (this instanceof VoronoiLayer) shift = 2;
+			if (biomeLayer==null) {
+				for (int offX = 0; offX < xSize; offX++) {
+					for (int offZ = 0; offZ < zSize; offZ++) {
+						this.layerCache.forceStoreAndGet(x+offX,0,z+offZ,this::sample);
+					}
+				}
+				return;
+			}
+
+			((IntBiomeLayer)biomeLayer)._sample((x>>shift)-1,(z>>shift)-1,xSize+2,zSize+2);
+			for (int offX = 0; offX < xSize; offX++) {
+				for (int offZ = 0; offZ < zSize; offZ++) {
+					this.layerCache.get(x+offX,0,z+offZ,this::sample);
+				}
+			}
+			return;
+		}
+	}
+
+
 	public int[] sample(int x, int y, int z, int xSize, int ySize, int zSize) {
-		throw new UnsupportedOperationException();
+		try {
+			this._sample(x,z,xSize,zSize);
+		} catch (UnexpectedException e) {
+			e.printStackTrace();
+		}
+		int[] ids=new int[xSize*zSize];
+		for (int offX = 0; offX < xSize; offX++) {
+			for (int offZ = 0; offZ < zSize; offZ++) {
+				ids[offX*zSize+offZ]=this.layerCache.get(x+offX,0,z+offZ,this::sample);
+			}
+		}
+		return ids;
 	}
 
 }
